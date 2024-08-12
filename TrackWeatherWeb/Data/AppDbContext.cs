174031ea3a3e312aaafcd59e504c8e99b6b917ca
Nikolai <marchenkokoly2004@mail.ru>
2024-08-12 19:25:09 +0300

    Add Data

---
+++ TrackWeatherWeb/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TrackWeatherWeb.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
+    {
+        public DbSet<ApplicationUsers> users { get; set; }
+        public DbSet<TransportRequests> requests { get; set; }
+
+    }
+}
