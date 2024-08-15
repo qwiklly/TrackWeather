c981186b87fb2c6e7bcbb89f9931fbdfd4360071
Nikolai <marchenkokoly2004@mail.ru>
2024-08-15 16:55:01 +0300

    Add Services

---
+++ TrackWeatherWeb/Services/ApplicationService.cs
@@ -0,0 +1,142 @@
+﻿using static TrackWeatherWeb.Responses.CustomResponses;
+using TrackWeatherWeb.DTOs;
+
+namespace TrackWeatherWeb.Services
+{
+    public class ApplicationService(HttpClient httpClient) : IApplicationService
+    {
+        private readonly HttpClient httpClient = httpClient;
+
+        public async Task<BaseResponse> GetUsersAsync()
+        {
+            var response = await httpClient.GetAsync("api/account/getUsers");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}"); //request not completed
+        }
+
+        public async Task<BaseResponse> GetUserAsync(string email)
+        {
+            var response = await httpClient.PostAsJsonAsync("api/account/GetUser", email);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+
+        public async Task<BaseResponse> GetCoordinatesAsync()
+        {
+            var response = await httpClient.GetAsync("api/account/getCoordinates");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+
+        public async Task<BaseResponse> GetCoordinateAsync(int id)
+        {
+            var response = await httpClient.PostAsJsonAsync("api/account/GetCoordinate", id);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+
+        public async Task<LoginResponse> LoginAsync(LoginDTO model)
+        {
+            var response = await httpClient.PostAsJsonAsync("api/account/login", model);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<LoginResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+
+        public async Task<BaseResponse> DeleteUserAsync(string email)
+        {
+            var response = await httpClient.DeleteAsync($"api/account/delete/{email}");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+
+        public async Task<BaseResponse> DeleteCoordinatesAsync(int id)
+        {
+            var response = await httpClient.DeleteAsync($"api/deleteCoordinates/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+
+        public async Task<BaseResponse> UpdateCoordinatesAsync(RequestTransportDTO model)
+        {
+            var response = await httpClient.PutAsJsonAsync("api/updateRequestTransport", model);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+
+        public async Task<BaseResponse> UpdateUserAsync(RegisterDTO model)
+        {
+            var response = await httpClient.PutAsJsonAsync("api/account/updateRegister", model);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+
+        public async Task<BaseResponse> Confirm_pointAsync(RequestTransportDTO model)
+        {
+            var response = await httpClient.PostAsJsonAsync("api/requestTransport", model);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<BaseResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+        public async Task<RegisterResponse> RegisterAsync(RegisterDTO model)
+        {
+            var response = await httpClient.PostAsJsonAsync("api/account/register", model);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<RegisterResponse>();
+                return result!;
+            }
+            else
+                throw new HttpRequestException($"Запрос не удался: {response.ReasonPhrase}");
+        }
+    }
+}
+
---
+++ TrackWeatherWeb/Services/IApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using static TrackWeatherWeb.Responses.CustomResponses;
+using TrackWeatherWeb.DTOs;
+
+namespace TrackWeatherWeb.Services
+{
+    public interface IApplicationService
+    {
+        Task<BaseResponse> GetUsersAsync();
+        Task<BaseResponse> GetCoordinatesAsync();
+        Task<RegisterResponse> RegisterAsync(RegisterDTO model);
+        Task<LoginResponse> LoginAsync(LoginDTO model);
+        Task<BaseResponse> Confirm_pointAsync(RequestTransportDTO model);
+        Task<BaseResponse> DeleteUserAsync(string email);
+        Task<BaseResponse> DeleteCoordinatesAsync(int id);
+        Task<BaseResponse> UpdateUserAsync(RegisterDTO model);
+        Task<BaseResponse> UpdateCoordinatesAsync(RequestTransportDTO model);
+    }
+}
+
