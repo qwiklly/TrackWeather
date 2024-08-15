848e91e7185579883d83c7faf0e6ea267226cd7b
Nikolai <marchenkokoly2004@mail.ru>
2024-08-15 13:30:38 +0300

    Add Responses

---
+++ TrackWeatherWeb/Responses/CustomResponses.cs
@@ -0,0 +1,11 @@
+﻿namespace TrackWeatherWeb.Responses
+{
+    public class CustomResponses
+    {
+        public record BaseResponse(bool Flag = false, string Message = null!);
+        public record RegisterResponse(bool Flag = false, string Message = null!) : BaseResponse(Flag,Message);
+        public record LoginResponse(bool Flag = false, string Message = null!, string JWTToken = null!) :BaseResponse(Flag,Message);
+        public record GenericResponse<T>(bool Flag = false, string Message = null!, T? Data = default) : BaseResponse(Flag, Message);
+
+    }
+}
