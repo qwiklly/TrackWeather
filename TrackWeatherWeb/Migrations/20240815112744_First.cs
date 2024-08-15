c2390e8b93eb6f26e3290a348053d76693600b41
Nikolai <marchenkokoly2004@mail.ru>
2024-08-15 16:58:12 +0300

    Add Migrations, update models

---
+++ TrackWeatherWeb/Migrations/20240815112744_First.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackWeatherWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Coordinate_x = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Coordinate_y = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- TrackWeatherWeb/Models/TransportRequests.cs
+++ TrackWeatherWeb/Models/TransportRequests.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
-        public decimal Coordinate_x { get; set; }
-        public decimal Coordinate_y { get; set; }
-        public string Description { get; set; } = string.Empty;
+        public decimal? Coordinate_x { get; set; }
+        public decimal? Coordinate_y { get; set; }
+        public string Comment { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; }
 
     }
