83b7f090027373dcae2a3d936cb20200a3249754
Nikolai <marchenkokoly2004@mail.ru>
2024-08-18 21:51:22 +0300

    Small refactor registerDTO

--- TrackWeatherWeb/DTOs/RegisterDTO.cs
+++ TrackWeatherWeb/DTOs/RegisterDTO.cs
@@ -4,12 +4,13 @@ namespace TrackWeatherWeb.DTOs
 {
     public class RegisterDTO : LoginDTO
     {
-        [Required]
-        public string Name {  get; set; } = string.Empty;
 
         [Required, Compare(nameof(Password)), DataType(DataType.Password)]
         public string ConfirmPassword { get; set; } = string.Empty;
 
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        
         [Required]
         public string Role { get; set; } = string.Empty;
 
