4a2b758e25c7e5b4dbb7e4a407218a1b5ed313c0
Nikolai <marchenkokoly2004@mail.ru>
2024-08-20 13:17:39 +0300

    fix errors in models

--- TrackWeatherWeb/Models/ApplicationUsers.cs
+++ TrackWeatherWeb/Models/ApplicationUsers.cs
@@ -6,7 +6,6 @@
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Role { get; set; }  = string.Empty;
-        public string Password = string.Empty;
-       
+        public string Password { get; set; } = string.Empty;   
     }
 }
--- TrackWeatherWeb/Models/TransportRequests.cs
+++ TrackWeatherWeb/Models/TransportRequests.cs
@@ -1,13 +1,16 @@
-﻿namespace TrackWeatherWeb.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrackWeatherWeb.Models
 {
     public class TransportRequests
     {
         public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 6)")]
         public decimal? Coordinate_x { get; set; }
+        [Column(TypeName = "decimal(18, 6)")]
         public decimal? Coordinate_y { get; set; }
         public string Comment { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; }
-
     }
 }
