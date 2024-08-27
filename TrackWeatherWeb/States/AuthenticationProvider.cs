090a23011b8bed221dd55fcf67294c25c6540ed8
Nikolai <marchenkokoly2004@mail.ru>
2024-08-27 21:26:10 +0300

    add token saving in cookies

--- TrackWeatherWeb/States/AuthenticationProvider.cs
+++ TrackWeatherWeb/States/AuthenticationProvider.cs
@@ -7,45 +7,91 @@ namespace TrackWeatherWeb.States
 {
     public class AuthenticationProvider : AuthenticationStateProvider
     {
-        //not authenticated(anonymous) 
+        private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly ClaimsPrincipal anonymous = new(new ClaimsIdentity());
-        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+
+        public AuthenticationProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             try
             {
-                if (string.IsNullOrEmpty(Constants.JWTToken))
+                var jwtToken = GetJwtTokenFromCookie();
+                if (string.IsNullOrEmpty(jwtToken))
                     return await Task.FromResult(new AuthenticationState(anonymous));
 
-                var getUserClaims = DecryptToken(Constants.JWTToken);
-                if (getUserClaims == null) return await Task.FromResult(new AuthenticationState(anonymous));
+                var getUserClaims = DecryptToken(jwtToken);
+                if (getUserClaims == null)
+                    return await Task.FromResult(new AuthenticationState(anonymous));
 
                 var claimsPrincipal = SetClaimPrincipal(getUserClaims);
                 return await Task.FromResult(new AuthenticationState(claimsPrincipal));
             }
-            catch { return await Task.FromResult(new AuthenticationState(anonymous)); }
+            catch
+            {
+                return await Task.FromResult(new AuthenticationState(anonymous));
+            }
         }
-        //Update token
+
         public async Task UpdateAuthenticationState(string? jwtToken)
         {
+            var claimsPrincipal = new ClaimsPrincipal();
+            if (!string.IsNullOrEmpty(jwtToken))
+            {
+                Constants.JWTToken = jwtToken;
+                var getUserClaims = DecryptToken(jwtToken);
+                claimsPrincipal = SetClaimPrincipal(getUserClaims);
+                await SetJwtTokenInCookie(jwtToken);
+            }
+            else
+            {
+                Constants.JWTToken = null!;
+                RemoveJwtTokenFromCookie();
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+
+        private async Task SetJwtTokenInCookie(string token)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true, // Используйте только с HTTPS
+                Expires = DateTime.UtcNow.AddDays(7),
+                SameSite = SameSiteMode.Strict
+            };
+
             await Task.Run(() =>
             {
-                var claimsPrincipal = new ClaimsPrincipal();
-                if (!string.IsNullOrEmpty(jwtToken))
-                {
-                    Constants.JWTToken = jwtToken;
-                    var getUserClaims = DecryptToken(jwtToken);
-                    claimsPrincipal = SetClaimPrincipal(getUserClaims);
-                }
-                else
-                {
-                    Constants.JWTToken = null!;
-                }
-                NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+                _httpContextAccessor.HttpContext?.Response.Cookies.Append("AuthToken", token, cookieOptions);
             });
         }
 
-        //parameters to login
-        public static ClaimsPrincipal SetClaimPrincipal(CustomUserClaims claims)
+
+        private string? GetJwtTokenFromCookie()
+        {
+            if (_httpContextAccessor.HttpContext?.Request.Cookies.TryGetValue("AuthToken", out string? token) == true)
+            {
+                return token;
+            }
+
+            return null;
+        }
+
+
+        private void RemoveJwtTokenFromCookie()
+        {
+            if (_httpContextAccessor.HttpContext?.Request.Cookies.ContainsKey("AuthToken") == true)
+            {
+                _httpContextAccessor.HttpContext.Response.Cookies.Delete("AuthToken");
+            }
+        }
+
+        private static ClaimsPrincipal SetClaimPrincipal(CustomUserClaims claims)
         {
             if (claims.Email is null) return new ClaimsPrincipal();
             return new ClaimsPrincipal(new ClaimsIdentity(
@@ -56,6 +102,7 @@ namespace TrackWeatherWeb.States
                     new(ClaimTypes.Role, claims.Role!),
                 }, "JwtAuth"));
         }
+
         private static CustomUserClaims DecryptToken(string jwtToken)
         {
             if (string.IsNullOrEmpty(jwtToken)) return new CustomUserClaims();
