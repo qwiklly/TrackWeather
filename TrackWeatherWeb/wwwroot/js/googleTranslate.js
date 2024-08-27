5cab09bb65ba2bd0daba641feb4f9605b3087b63
Nikolai <marchenkokoly2004@mail.ru>
2024-08-27 21:27:49 +0300

    add translate script

---
+++ TrackWeatherWeb/wwwroot/js/googleTranslate.js
@@ -0,0 +1,14 @@
+﻿function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        { pageLanguage: 'ru', includedLanguages: 'en,ru', autoDisplay: false },
+        'google_translate_element'
+    );
+}
+
+function translatePage() {
+    var googleTranslate = document.querySelector('.goog-te-combo');
+    if (googleTranslate) {
+        googleTranslate.value = 'en';
+        googleTranslate.dispatchEvent(new Event('change'));
+    }
+}
