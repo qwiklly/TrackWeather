d1fd5aa30e134f34e5932d92e5650a7e56289a39
Nikolai <marchenkokoly2004@mail.ru>
2024-08-28 17:37:56 +0300

    Add registration page for users

---
+++ TrackWeatherWeb/Pages/Registration.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TrackWeatherWeb.DTOs;
+using TrackWeatherWeb.HttpServices;
+
+namespace TrackWeatherWeb.Pages
+{
+    public class RegistrationModel : PageModel
+    {
+        [BindProperty]
+        public RegisterDTO Register { get; set; } = new RegisterDTO();
+
+        public bool IsUserAdded { get; set; } = false;
+
+        private readonly IApplicationService _accountService;
+
+        public RegistrationModel(IApplicationService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public void OnGet()
+        {
+            // clear temp after reloading page
+            TempData["SuccessMessage"] = null;
+            TempData["ErrorMessage"] = null;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+			if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+			var response = await _accountService.RegisterAsync(Register);
+
+            if (!response.Flag)
+            {
+                TempData["ErrorMessage"] = response.Message;
+                return Page();
+            }
+
+            IsUserAdded = true;  
+            return Page(); 
+        }
+    }
+}
