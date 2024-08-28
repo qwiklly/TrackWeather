3a1d1b95854b7d41bdd01696723008f99b12af05
Nikolai <marchenkokoly2004@mail.ru>
2024-08-28 17:17:40 +0300

    Add admin page ( add users )

---
+++ TrackWeatherWeb/Pages/AddUser.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TrackWeatherWeb.DTOs;
+using TrackWeatherWeb.HttpServices;
+
+namespace TrackWeatherWeb.Pages
+{
+    public class AddUserModel : PageModel
+    {
+        [BindProperty]
+        public RegisterDTO Register { get; set; } = new RegisterDTO();
+
+        public bool IsUserAdded { get; set; } = false;
+
+        private readonly IApplicationService _accountService;
+
+        public AddUserModel(IApplicationService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public void OnGet()
+        {
+            TempData["SuccessMessage"] = null;
+            TempData["ErrorMessage"] = null;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var response = await _accountService.RegisterAsync(Register);
+
+            if (!response.Flag)
+            {
+                TempData["ErrorMessage"] = response.Message;
+                return Page();
+            }
+
+            IsUserAdded = true;  
+            return Page(); 
+        }
+    }
+}
