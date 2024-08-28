682badba98d9fa8ad2cee92a0ee478e90d905c7e
Nikolai <marchenkokoly2004@mail.ru>
2024-08-28 17:37:26 +0300

    Update info about project on index page

--- TrackWeatherWeb/Pages/Index.cshtml.cs
+++ TrackWeatherWeb/Pages/Index.cshtml.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TrackWeatherWeb.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(AuthenticationStateProvider authStateProvider) : PageModel
     {
-		public IndexModel()
-		{
-		}
+        private readonly AuthenticationStateProvider _authStateProvider = authStateProvider;
+        public string? UserName { get; private set; }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-        }		
-	}
+            var authState = await _authStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            // Если пользователь авторизован, берем его имя
+            if (user.Identity!.IsAuthenticated)
+            {
+                UserName = user.Identity.Name!;
+            }
+        }
+    }
 }
